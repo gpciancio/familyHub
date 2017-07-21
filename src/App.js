3f5d244d4794e2f8a7042c5f2b1f07c4d2926838
Zach Passarelli <zachary.passarelli@gmail.com>
2017-07-21 15:16:07 -0600

    add components folder, add basic structure to app.js, add calendar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
+import Calendar from './components/calendar';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        
+      <div>
+        <div id="calendar">
+          <Calendar />
+        </div>
+
+        <div id="weather">
+
+        </div>
+
+        <div id="whiteboard">
+
+        </div>
+
+        <div id="todo">
+
+        </div>
       </div>
     );
   }
