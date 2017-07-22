6ac52e0005b419a0289c69788de4ca9e0c342062
Zach Passarelli <zachary.passarelli@gmail.com>
2017-07-22 11:32:56 -0600

    whiteboard progress

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React, { Component } from 'react';
 import Calendar from './components/calendar';
+import Whiteboard from './components/whiteboard';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <div id="calendar">
+        <div id="calendar" className="calendar">
           <Calendar />
         </div>
 
-        <div id="weather">
+        <div id="weather" className="weather">
 
         </div>
 
-        <div id="whiteboard">
-
+        <div id="whiteboard" className="whiteboard">
+          <Whiteboard />
         </div>
 
-        <div id="todo">
-
+        <div id="todo" className="todo">
+          
         </div>
       </div>
     );
--- src/components/whiteboard.js
+++ src/components/whiteboard.js
@@ -1,17 +1,98 @@
 import React from 'react';
+import Immutable from 'immutable';
 
 class Whiteboard extends React.Component {
   constructor(props){
     super(props);
     this.state = {
+      lines: new Immutable.List(),
+      isDrawing: false
+    };
 
+    this.handleMouseDown = this.handleMouseDown.bind(this);
+    this.handleMouseMove = this.handleMouseMove.bind(this);
+    this.handleMouseUp = this.handleMouseUp.bind(this);
+  }
+
+  componentDidMount() {
+    document.addEventListener("mouseup", this.handleMouseUp);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("mouseup", this.handleMouseUp);
+  }
+
+  handleMouseDown(mouseEvent) {
+    if (mouseEvent.button !== 0) {
+      return;
     }
-  };
+
+    const point = this.relativeCoordinatesForEvent(mouseEvent);
+
+    this.setState(prevState => ({
+      lines: prevState.lines.push(new Immutable.List([point])),
+      isDrawing: true
+    }));
+  }
+
+  handleMouseMove(mouseEvent) {
+    if (!this.state.isDrawing) {
+      return;
+    }
+
+    const point = this.relativeCoordinatesForEvent(mouseEvent);
+
+    this.setState(prevState =>  ({
+      lines: prevState.lines.updateIn([prevState.lines.size - 1], line => line.push(point))
+    }));
+  }
+
+  handleMouseUp() {
+    this.setState({ isDrawing: false });
+  }
+
+  relativeCoordinatesForEvent(mouseEvent) {
+    const boundingRect = this.refs.drawArea.getBoundingClientRect();
+    console.log(boundingRect);
+    return new Immutable.Map({
+      x: mouseEvent.clientX - boundingRect.left,
+      y: mouseEvent.clientY - boundingRect.top,
+    });
+  }
 
   render() {
     return (
-      <div id="container"></div>
-    )
+      <div
+        className="drawArea"
+        ref="drawArea"
+        onMouseDown={this.handleMouseDown}
+        onMouseMove={this.handleMouseMove}
+      >
+        <Drawing lines={this.state.lines} />
+      </div>
+    );
   }
-
 }
+
+function Drawing({ lines }) {
+  return (
+    <svg className="drawing">
+      {lines.map((line, index) => (
+        <DrawingLine key={index} line={line} />
+      ))}
+    </svg>
+  );
+}
+
+function DrawingLine({ line }) {
+  const pathData = "M " +
+    line
+      .map(p => {
+        return `${p.get('x')} ${p.get('y')}`;
+      })
+      .join(" L ");
+
+  return <path className="path" d={pathData} />;
+}
+
+export default Whiteboard;
