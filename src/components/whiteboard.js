e90259227ea858f5c74cb86c9a5b8c974c26d1a2
Zach Passarelli <zachary.passarelli@gmail.com>
2017-07-22 10:21:10 -0600

    add immutable dependency

---
+++ src/components/whiteboard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Whiteboard extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+
+    }
+  };
+
+  render() {
+    return (
+      <div id="container"></div>
+    )
+  }
+
+}
