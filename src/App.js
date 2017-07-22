32e21c7a6ce48e8f8a21820792565aad91001ae7
Linda Call <lindacall4home@hotmail.com>
2017-07-22 11:48:37 -0600

    move state from app to todo list container

--- src/App.js
+++ src/App.js
@@ -3,60 +3,6 @@ import Calendar from './components/calendar';
 import TodoContainer from './components/todo-container';
 
 class App extends Component {
-  constructor(props) {
-     super(props);
-
-     this.state = {
-       items: [],
-       text: ""
-     };
-
-     this.handleTextChange = this.handleTextChange.bind(this);
-     this.handleAddItem = this.handleAddItem.bind(this);
-     this.markItemCompleted = this.markItemCompleted.bind(this);
-     this.handleDeleteItem = this.handleDeleteItem.bind(this);
-   }
-   handleTextChange(event) {
-     this.setState({
-       text: event.target.value
-     });
-   }
-   handleAddItem(event) {
-     event.preventDefault();
-
-     var newItem = {
-       id: Date.now(),
-       text: this.state.text,
-       done: false
-     };
-
-     this.setState((prevState) => ({
-       items: prevState.items.concat(newItem),
-       text: ""
-     }));
-   }
-   markItemCompleted(itemId) {
-     var updatedItems = this.state.items.map(item => {
-       if (itemId === item.id)
-         item.done = !item.done;
-
-       return item;
-     });
-
-     // State Updates are Merged
-     this.setState({
-       items: [].concat(updatedItems)
-     });
-   }
-   handleDeleteItem(itemId) {
-     var updatedItems = this.state.items.filter(item => {
-       return item.id !== itemId;
-     });
-
-     this.setState({
-       items: [].concat(updatedItems)
-     });
-   }
 
   render() {
     return (
@@ -74,10 +20,7 @@ class App extends Component {
         </div>
 
         <div id="todo">
-          <TodoContainer
-            items={this.state.items}
-            onItemCompleted={this.markItemCompleted} onDeleteItem={this.handleDeleteItem}
-          />
+          <TodoContainer/>
         </div>
       </div>
     );
--- src/components/todo-container.js
+++ src/components/todo-container.js
@@ -2,6 +2,60 @@ import React from 'react';
 import TodoList from './todo-list';
 
 class TodoContainer extends React.Component {
+  constructor(props) {
+     super(props);
+
+     this.state = {
+       items: [],
+       text: ""
+     };
+
+     this.handleTextChange = this.handleTextChange.bind(this);
+     this.handleAddItem = this.handleAddItem.bind(this);
+     this.markItemCompleted = this.markItemCompleted.bind(this);
+     this.handleDeleteItem = this.handleDeleteItem.bind(this);
+   }
+   handleTextChange(event) {
+     this.setState({
+       text: event.target.value
+     });
+   }
+   handleAddItem(event) {
+     event.preventDefault();
+
+     var newItem = {
+       id: Date.now(),
+       text: this.state.text,
+       done: false
+     };
+
+     this.setState((prevState) => ({
+       items: prevState.items.concat(newItem),
+       text: ""
+     }));
+   }
+   markItemCompleted(itemId) {
+     var updatedItems = this.state.items.map(item => {
+       if (itemId === item.id)
+         item.done = !item.done;
+
+       return item;
+     });
+
+     // State Updates are Merged
+     this.setState({
+       items: [].concat(updatedItems)
+     });
+   }
+   handleDeleteItem(itemId) {
+     var updatedItems = this.state.items.filter(item => {
+       return item.id !== itemId;
+     });
+
+     this.setState({
+       items: [].concat(updatedItems)
+     });
+   }
 
 
   render() {
@@ -11,9 +65,9 @@ class TodoContainer extends React.Component {
         <div className="row">
           <div className="col-md-3">
             <TodoList
-              items={this.props.items}
-              onItemCompleted={this.props.markItemCompleted} onDeleteItem={this.props.handleDeleteItem}
-            />
+              items={this.state.items}
+              onItemCompleted={this.markItemCompleted} onDeleteItem={this.handleDeleteItem}
+            />            
           </div>
         </div>
         <form className="row">
@@ -21,13 +75,13 @@ class TodoContainer extends React.Component {
             <input type="text"
               className="form-control"
               onChange={this.handleTextChange}
-              value={this.props.text} />
+              value={this.state.text} />
           </div>
           <div className="col-md-3">
             <button
               className="btn btn-primary"
               onClick={this.handleAddItem}
-              disabled={!this.props.text}>{"Add #" + (this.props.items.length + 1)}
+              disabled={!this.state.text}>Add
             </button>
           </div>
         </form>
