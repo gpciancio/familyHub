2f0ad6e6aee21554d5770f880d233153136197b0
Linda Call <lindacall4home@hotmail.com>
2017-07-25 15:18:11 -0600

    conditional splashpage with email saved in local storage

--- src/App.js
+++ src/App.js
@@ -6,32 +6,80 @@ import Whiteboard from './components/whiteboard';
 import TodoContainer from './components/todo-container';
 
 class App extends Component {
+  state = {
+    email: "familyhub123@gmail.com",
+    showSplash: true
+  }
+
+
+
+  componentDidMount = () => {
+    this.loadState();
+  }
+
+  updateEmailText = (newEmail) => {
+    this.setState({email: newEmail});
+  }
+
+  saveEmail = () => {
+    this.setState({showSplash: false});
+    this.saveState();
+  }
+
+
+  saveState = () => {
+    localStorage.setItem('famhub-email', this.state.email);
+  }
+
+  loadState = () => {
+   const email = localStorage.getItem('famhub-email');
+   if (email !== null) {
+
+     this.setState({
+       email: email
+     });
+   }
+
+   return;
+ }
+
 
   render() {
     return (
       <div>
         <div id="splash" className="splash">
-          <Splash />
+          <Splash
+            defaultEmail={this.state.email}
+            showSplash={this.state.showSplash}
+            updateEmailText={this.updateEmailText}
+            saveEmail={this.saveEmail}
+          />
         </div>
+
 
         <div id="calendar" className="calendar">
-          <Calendar />
+          <Calendar
+            email={this.state.email}
+          />
         </div>
+          <div id="weather" className="weather">
+            <Weather />
+          </div>
 
-        <div id="weather" className="weather">
-          <Weather />
-        </div>
+          <div id="whiteboard" className="whiteboard">
+            <Whiteboard />
+          </div>
 
-        <div id="whiteboard" className="whiteboard">
-          <Whiteboard />
-        </div>
+          <div id="todo" className="todo">
+            <TodoContainer/>
+          </div>
 
-        <div id="todo" className="todo">
-          <TodoContainer/>
-        </div>
       </div>
     );
   }
 }
+
+
+
 
 export default App;
--- src/components/splash.js
+++ src/components/splash.js
@@ -1,26 +1,34 @@
 import React from 'react';
 
 class Splash extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-    };
-  }
 
   render() {
-    return (
+
+    return  this.props.showSplash ? (
       <div className="splash_container">
         <div className="stage col-md-4 col-md-offset-4 col-xs-8 col-xs-offset-2 text-center">
           <span className="famhubtitle">Welcome to Family Hub!</span>
           <form className="form-inline">
             <div className="form-group form-group-lg emailform text-center">
-              <input type="email" className="form-control form-control-lg" id="email" placeholder="Enter Family Gmail"></input>
+              <input
+                type="email"
+                className="form-control form-control-lg"
+                id="email"
+                placeholder="Enter Family Gmail" value={this.props.defaultEmail}
+                onChange={ (event) => this.props.updateEmailText(event.target.value) }
+              ></input>
             </div>
-            <button type="submit" className="btn btn-default btn-lg">Enter</button>
+            <button
+              type="submit"
+              className="btn btn-default btn-lg"
+              onClick={this.props.saveEmail}
+            >Enter
+            </button>
           </form>
         </div>
       </div>
-    )
+    ) :
+    null;
   }
 }
 
